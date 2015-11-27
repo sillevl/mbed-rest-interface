dec48c79db5f5c69568e182d0a4627068c0ac126
Sille Van Landschoot <info@sillevl.be>
2015-11-27 09:54:30 +0100

    add api calls for led and temperatur

--- app.js
+++ app.js
@@ -9,6 +9,19 @@ app.get('/', function (req, res) {
   res.render('home');
 });
 
+app.get('/api/led', function (req, res) {
+  console.log("led GET");
+  res.write('{"color": "00FF00"}');
+});
+
+app.post('/api/led', function (req, res) {
+  console.log("led POST");
+});
+
+app.get('/api/temperature', function (req, res) {
+  res.render('{"temperature": "20.3"}');
+});
+
 var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
