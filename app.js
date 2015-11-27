008bdbef18aa056d80474a79eae0deb194418da6
Sille Van Landschoot <info@sillevl.be>
2015-11-27 09:30:11 +0100

    add handlebars boilerplate code

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var app = express();
+var exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.render('home');
 });
 
 var server = app.listen(3000, function () {
